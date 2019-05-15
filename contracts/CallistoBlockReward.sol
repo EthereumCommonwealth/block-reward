20a3ef45ace9591fc4309d780cf7c7d5f658de7c
Yohan Graterol <yohangraterol92@gmail.com>
2019-05-15 00:51:01 -0500

    Smart contract reward for Monetary policy

--- contracts/CallistoBlockReward.sol
+++ contracts/CallistoBlockReward.sol
@@ -27,15 +27,22 @@ interface BlockReward {
 contract CallistoBlockReward is BlockReward {
     address constant SYSTEM_ADDRESS = 0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE;
 
-    uint256 constant MINER_REWARD = 0x16c4abbebea0100000;
+    // uint256 constant MINER_REWARD = 0x16c4abbebea0100000;
     uint256 constant STAKE_REWARD = 0x340aad21b3b700000;
     address constant STAKE_ADDRESS = 0x3c06f218Ce6dD8E2c535a8925A2eDF81674984D9;
-    address constant STAKE_ADDRESS_HF1 = 0xd813419749b3c2cdc94a2f9cfcf154113264a9d6;
+    address constant STAKE_ADDRESS_HF1 = 0xd813419749b3c2cDc94A2F9Cfcf154113264a9d6;
     uint256 constant TREASURY_REWARD = 0x68155a43676e00000;
     address constant TREASURY_ADDRESS = 0x74682Fc32007aF0b6118F259cBe7bCCC21641600;
     uint256 constant TREASURY_REWARD_HF1 = STAKE_REWARD;
     uint256 constant STAKE_REWARD_HF1 = TREASURY_REWARD;
     uint256 constant HF1_BLOCK = 0x155cc0;
+
+    // Monetary policy
+    uint256[5] MP_MINER_REWARD = [0x16c4abbebea0100000, 0xcaf67003701680000, 0x7068fb1598aa00000, 0x3dd356e57a5d80000, 0x21b91820143300000];
+
+    uint256[5] MP_STAKE_REWARD = [0x68155a43676e00000, 0x1f399b1438a100000, 0x12bc29d8eec700000, 0xb3db2b55c1100000, 0x6bd495d530c90000];
+
+    uint256[5] MP_TREASURY_REWARD = [0x340aad21b3b700000, 0x4e1003b28d9280000, 0x38347d8acc5500000, 0x2757f17ac23b80000, 0x1af996539a2a60000];
 
     modifier onlySystem {
         require(msg.sender == SYSTEM_ADDRESS);
@@ -53,12 +60,14 @@ contract CallistoBlockReward is BlockReward {
 
         address[] memory addresses = new address[](3); // minimum 3 for author, ubi contract and dev contract
         uint256[] memory rewards = new uint256[](3);
+        uint256 blockPosition = getBlockPosition();
+        uint256 MINER_REWARD = getMinerReward(blockPosition);
 
         addresses[1] = getStakeAddress();
-        rewards[1] = getStakeReward();
+        rewards[1] = getStakeReward(blockPosition);
 
         addresses[2] = TREASURY_ADDRESS;
-        rewards[2] = getTreasuryReward();
+        rewards[2] = getTreasuryReward(blockPosition);
 
         for (uint i = 0; i < beneficiaries.length; i++) {
             if (kind[i] == 0) { // author
@@ -109,31 +118,56 @@ contract CallistoBlockReward is BlockReward {
         view
         returns (address)
     {
-        if (block.number >= HF1_BLOCK) {
+        if (block.number > HF1_BLOCK) {
             return STAKE_ADDRESS_HF1;
         }
         return STAKE_ADDRESS;
     }
 
-    function getTreasuryReward()
+    function getTreasuryReward(uint256 blockPosition)
         internal
         view
         returns (uint256)
     {
-        if (block.number >= HF1_BLOCK) {
-            return TREASURY_REWARD_HF1;
+        if (block.number > HF1_BLOCK) {
+            return MP_TREASURY_REWARD[blockPosition];
         }
         return TREASURY_REWARD;
     }
 
-    function getStakeReward()
+    function getStakeReward(uint256 blockPosition)
         internal
         view
         returns (uint256)
     {
-        if (block.number >= HF1_BLOCK) {
-            return STAKE_REWARD_HF1;
+        if (block.number > HF1_BLOCK) {
+            return MP_STAKE_REWARD[blockPosition];
         }
         return STAKE_REWARD;
     }
+
+    function getMinerReward(uint blockPosition)
+        internal
+        view
+        returns (uint256)
+    {
+        return MP_MINER_REWARD[blockPosition];
+    }
+
+    function getBlockPosition()
+        internal
+        view
+        returns (uint256)
+    {
+        if (block.number < 2750001) {
+            return 0;
+        } else if (block.number < 4250001) {
+            return 1;
+        } else if (block.number < 5750001) {
+            return 2;
+        } else if (block.number < 7250001) {
+            return 3;
+        }
+        return 4;
+    }    
 }
